e4bf1a4c18dc21dc9b1b9237b168332c4f45f507
labyrinth-ssr <20302010059@fudan.edu.cn>
2021-04-21 18:21:17 +0800

    lab3 to lab 4

--- lab11.cpp
+++ lab11.cpp
@@ -1,24 +1,68 @@
-#include <stdio.h>
+#include <cstdlib>
+#include <ctime>
+#include <cstring>
+#include <algorithm>
+#include <sstream>
+#include "my_class.h"
 #include <iostream>
+#include <vector>
 #include <string>
-#include <string.h>
-#include <stdlib.h>
-#include <time.h>
-#include <conio.h>
-#include <Windows.h>
+#include <getopt.h>
 
 using namespace std;
-int score=0,score1=0,score2=0;
-int mode=1;
-int c=1;
-int player=0;
-string username1,username2;
+using namespace className;
 
-void printMap(int str[][4]){//带清屏的打印
-    system("cls");
-    for (int i=0;i<4;i++){
-        printf("+-----+-----+-----+-----+\n");
-        for (int j=0;j<4;j++){
+void Map::judgeWinner(){
+        if (getMode() ==2){
+            if (score[1]>score[2])
+            cout<<username[0]<<"win!"<<endl;
+            else if (score[1]==score[2])
+            cout<<"draw!"<<endl;
+            else
+            cout<<username[1]<<"win!"<<endl;
+        }
+}
+
+void Map::setUsername(){
+        cout<<"Input user1's name:";
+        cin>>username[0];
+        cout<<"Input user2's name:";
+        cin>>username[1];
+        cout<<username[0]<<" "<<username[1]<<endl;
+}
+
+void Map::getTurnAddPrintScore(){
+    if (playerNumber==1){
+        cout<<"The score is:"<<score[0]<<endl;
+    }
+    else{
+            cout<<"the next player is"<<username[2-player]<<endl;
+            cout<<username[player-1]<<"'s score is:"<<score[player]<<endl;
+    }
+}
+
+void Map::setMode(){
+    cin>>playerNumber;
+    cout<<"the mode is:"<<playerNumber<<endl;
+}
+
+void Map::add_score(int i,int j){
+    score[player]+=str[i][j];
+}
+
+void printInterface(){      
+    printf("--------------game begins--------------\n\n"
+    "please choose mode:\n1:one player\n2:two players\n"
+    "please input the number:");
+}
+
+void Map:: printMap() const{//带清屏的打印
+    for (int i=0;i<length;i++){
+        for (int j=0;j<length;j++){
+            cout<<"+-----";
+        }
+        cout<<"+"<<endl;
+        for (int j=0;j<length;j++){
             if(str[i][j]==0)
             printf("|     ");
             else{
@@ -26,122 +70,100 @@ void printMap(int str[][4]){//带清屏的打印
             }
         }printf("|\n");
     }
-    printf("+-----+-----+-----+-----+\n");
-    switch(player){
-        case 0:
-        cout<<"score:"<<score;
-        break;
-        case 1:
-        cout<<"the next player is:"<<username2<<endl;
-        cout<<username1<<"'s current score :"<<score1;
-        break;
-        case 2:
-        cout<<"the next player is:"<<username1<<endl;
-        cout<<username2<<"'s current score :"<<score2;
-        break;
+    for (int j=0;j<length;j++){
+        cout<<"+-----";
     }
+    cout<<"+"<<endl;
+    
 }
 
-void getRandom(int str[4][4]){
+void Map:: getRandom(){
+    srand((unsigned)time(NULL));
     int x,y;
-    while(str[x=rand()%4][y=rand()%4]!=0){
+    while(str[x=rand()%length][y=rand()%length]!=0){
         ;
     }
     str[x][y]=2;
 }
 
-void iniMap(int str[4][4]){
-    for (int i=0;i<4;i++){
-        for (int j=0;j<4;j++){
+void Map:: iniMap(){
+    for (int i=0;i<length;i++){
+        for(int j=0;j<length;j++){
             str[i][j]=0;
         }
     }
-    getRandom(str);
-    getRandom(str);
+    getRandom();
+    getRandom();
 }
 
-void add_score(int i,int j,int str[4][4]){
-        switch(player){
-        case 0:
-        score += str[i][j];
-        break;
-        case 1:
-        score1 += str[i][j];
-        break;
-        case 2:
-        score2 += str[i][j];
-        break;
-    }
-}
-
-void mergew(int str[4][4],int i=0,int j=0){
-    for ( j=0;j<4;j++){
-        for ( i=0;i<3;i++){
+void Map:: mergew(){
+    for (int j=0;j<length;j++){
+        for (int i=0;i<length-1;i++){
             if(str[i][j]==str[i+1][j]){
                 str[i][j] *=2;
-                add_score(i,j,str);
+                add_score(i,j);
                 str[i+1][j] =0;
             }
         }
     }
 }
 
-void mergea(int str[4][4]){
-    for (int j=0;j<4;j++){
-        for (int i=0;i<3;i++){
+void Map:: mergea(){
+    for (int j=0;j<length;j++){
+        for (int i=0;i<length-1;i++){
             if(str[j][i]==str[j][i+1]){
                 str[j][i] *=2;
-                add_score(j,i,str);
+                add_score(j,i);
                 str[j][i+1] =0;
             }
         }
     }
 }
 
-void mergez(int str[4][4]){
-    for (int j=0;j<4;j++){
-        for (int i=3;i>0;i--){
+void Map:: mergez(){
+    for (int j=0;j<length;j++){
+        for (int i=length-1;i>0;i--){
             if(str[i][j]==str[i-1][j]){
                 str[i][j] *=2;
-                add_score(i,j,str);
+                add_score(i,j);
                 str[i-1][j] =0;
             }
         }
     }
 }
 
-void merges(int str[4][4]){
-    for (int j=0;j<4;j++){
-        for (int i=3;i>0;i--){
+void Map:: merges(){
+    for (int j=0;j<length;j++){
+        for (int i=length-1;i>0;i--){
             if(str[j][i]==str[j][i-1]){
                 str[j][i] *=2;
-                add_score(j,i,str);
+                add_score(j,i);
                 str[j][i-1] =0;
             }
         }
     }
 }
 
-void movew(int str[4][4]){
-    for (int j=0;j<4;j++){
-        for (int i=0;i<3;i++){
+void Map:: movew(){
+    for (int j=0;j<length;j++){
+        for (int i=0;i<length-1;i++){
             int k=i;
-            while(k<4&&str[k][j]==0){
+            while(k<length&&str[k][j]==0){
                 k++;
             }
-            for (int l=i,m=k;m<4;l++,m++){
+            for (int l=i,m=k;m<length;l++,m++){
                 str[l][j]=str[m][j];
             }
             for (int l=0;l<k-i;l++){
-                str[3-l][j]=0;
+                str[length-1-l][j]=0;
             }
         }
     }
 }
 
-void movez(int str[4][4]){
-    for (int j=0;j<4;j++){
-        for (int i=3;i>=0;i--){
+void Map:: movez(){
+    for (int j=0;j<length;j++){
+        for (int i=length-1;i>=0;i--){
             int k=i;
             while(k>=0&&str[k][j]==0){
                 k--;
@@ -149,33 +171,33 @@ void movez(int str[4][4]){
             for (int l=i,m=k;m>=0;l--,m--){
                 str[l][j]=str[m][j];
             }
-            for (int l=3;l>3-i+k;l--){
-                str[3-l][j]=0;
+            for (int l=length-1;l>length-1-i+k;l--){
+                str[length-1-l][j]=0;
             }
         }
     }
 }
 
-void movea(int str[4][4]){
-    for (int j=0;j<4;j++){
-        for (int i=0;i<3;i++){
+void Map:: movea(){
+    for (int j=0;j<length;j++){
+        for (int i=0;i<length-1;i++){
             int k=i;
-            while(k<4&&str[j][k]==0){
+            while(k<length&&str[j][k]==0){
                 k++;
             }
-            for (int l=i,m=k;m<4;l++,m++){
+            for (int l=i,m=k;m<length;l++,m++){
                 str[j][l]=str[j][m];
             }
             for (int l=0;l<k-i;l++){
-                str[j][3-l]=0;
+                str[j][length-1-l]=0;
             }
         }
     }
 }
 
-void moves(int str[4][4]){
-    for (int j=0;j<4;j++){
-        for (int i=3;i>=0;i--){
+void Map:: moves(){
+    for (int j=0;j<length;j++){
+        for (int i=length-1;i>=0;i--){
             int k=i;
             while(k>=0&&str[j][k]==0){
                 k--;
@@ -183,168 +205,223 @@ void moves(int str[4][4]){
             for (int l=i,m=k;m>=0;l--,m--){
                 str[j][l]=str[j][m];
             }
-            for (int l=3;l>3-i+k;l--){
-                str[j][3-l]=0;
+            for (int l=length-1;l>length-1-i+k;l--){
+                str[j][length-1-l]=0;
             }
         }
     }
 }
 
-void save_func(int str[4][4],int save[4][4]){
-    for (int i=0;i<4;i++){
-        for (int j=0;j<4;j++){
-            save[i][j]=str[i][j];
-        }
-    }
-}
-
-bool check_equal(int str[4][4],int save[4][4]){
-    for (int i=0;i<4;i++){
-        for (int j=0;j<4;j++){
-            if(save[i][j]!=str[i][j])
-                return 0;
-        }
-    }
-    return 1;
-}
-
-bool check_win(int str[4][4],int condition){
-    for (int i=0;i<4;i++){
-        for (int j=0;j<4;j++){
-            if (str[i][j]==condition)
+bool Map::check_win(){
+    for (int i=0;i<length;i++){
+        for (int j=0;j<length;j++){
+            if (str[i][j]==winCondition)
                 return true;
         }
     }
     return false;
 }
 
-void updateWithInput(int str[4][4],char input[5],int i){
-    int save[4][4]={0};
+void Map::updateWithInput(string input,int i){
+    vector<vector<int>> copy=str;
     switch (input[i]){
         case 'w':
-        save_func(str,save);
-        movew(str);   
-        mergew(str);
-        movew(str);
-        if (check_equal(str,save)) break;
-        getRandom(str);
+        movew();   
+        mergew();
+        movew();
+        if (copy==str) break;
+        getRandom();
         break;
         case 'z':
-        save_func(str,save);
-        movez(str);
-        mergez(str);
-        movez(str);
-        if (check_equal(str,save)) break;
-        getRandom(str);
+        movez();
+        mergez();
+        movez();
+        if (copy==str) break;
+        getRandom();
         break;
         case 'a':
-        save_func(str,save);
-        movea(str);
-        mergea(str);
-        movea(str);
-        if (check_equal(str,save)) break;
-        getRandom(str);
+        movea();
+        mergea();
+        movea();
+        if (copy==str) break;
+        getRandom();
         break;
         case 's':
-        save_func(str,save);
-        moves(str);
-        merges(str);
-        moves(str);
-        if (check_equal(str,save)) break;
-        getRandom(str);
+        moves();
+        merges();
+        moves();
+        if (copy==str) break;
+        getRandom();
     }
+    addCount();
+    
 }
 
-bool is_full(int str[4][4]){
-    for (int i=0;i<4;i++){
-        for (int j=0;j<4;j++){
-            if (str[i][j]==0) return false;
+bool Map::is_full(){
+    for (int i=0;i<length;i++){
+        for (int j=0;j<length;j++){
+            if (str[i][j]==0){
+                return false;
+            }
         }
     }
     return true;
 }
 
-bool check_lose(int str[4][4]){
-    int save0[4][4]={0};
-    save_func(str,save0);
-    char input[5]={'w','a','z','s'};
-    if (!is_full(str)) return false;
-    else{
+bool Map::check_lose(){
+    vector<vector<int>> copy1=str;
+    vector<int> saveScore=score;
+    string input="wazs";
         for (int i=0;i<4;i++){
-        updateWithInput(str,input,i);
-        if (!check_equal(str,save0)) {
-            save_func(save0,str);
-            return false;
+            updateWithInput(input,i);
+            if (copy1==str) {
+                direction[i]='\0';//the direction can't be moved.
+            }
+            else{
+                str=copy1;
+            }
         }
-    }
-    }
+        score=saveScore;
+        cout<<"This direction can be moved";
+        for(int i=0;i<4;i++){
+            cout<<direction[i]<<" ";
+        }
+        cout<<endl;
+        for (int i=0;i<4;i++){
+                if(direction[i]!='\0') {
+                    return false;
+                }
+        }
     return true;
 }
 
-void print_interface(){
-    printf("--------------game begins--------------\n\n"
-    "please choose mode:\n1:one player\n2:two players\n"
-    "please input the number:");
-    cin>>mode;
-    if (mode==2){
-        cout<<"please input user1's name:";
-        cin>>username1;
-        cout<<"please input user2's name:";
-        cin>>username2;
+void Map::judgePlayer(){
+    if (playerNumber==2){
+        if(gameCount%2==1) player=1;
+        else player=2;
+    }
+    cout<<"Now the player is:"<<player<<endl;
+}
+
+void Map::getDemand(){
+    string save1;//take in direction
+    for(int i=0;i<4;i++){
+        if(direction[i]=='w'||direction[i]=='a'||direction[i]=='z'||direction[i]=='s'){
+           save1.push_back(direction[i]);
+        }
+        
+    }
+    cout<<"check:these directions can be moved"<<save1<<endl;
+    cout<<"the size of the save1 is:"<<save1.size()<<endl;
+    if(save1.size()==1){
+        demand=save1[0];
+        cout<<"get demand:"<<demand<<endl;
     }
 }
 
-void judge_player(){
-    if (mode==1){
-        player=0;
+void Map::checkCheated(string& input){//find the condition
+    if (cheatMode&&player==cheated){
+        getDemand();
+        if(demand!='\0'){
+            cout<<word[1]<<" if agree,please input"<<demand;
+            cheatMode=false;
+            haveCheated=true;
+            cin>>input;
+            word[0]='\0';
+            while(input[0]!=demand){
+                cin>>input;
+            }
+        }
+        else{
+            word[0]='\0';
+            cin>>input;
+        }
     }
     else{
-        if (c%2==1) player=1;
-        else player=2;
+        word[0]='\0';
+        getline(cin,input);
+        istringstream record(input);
+        int i=0;
+        while(record>>word[i]){
+            cout<<word[i++]<<" "<<endl;
+        }
+    }
+    direction[0]='w';direction[1]='a';direction[2]='z';direction[3]='s';
+}
+
+void Map::getCheat(string& input){
+    if(!haveCheated){
+    if(word[0]=="c"){
+        cheatMode=true;
+        if(player==1) cheated=2;
+        else cheated=1;
+        cout<<"cheat effect open"<<endl;
+        cout<<cheated<<"is cheated"<<endl;
+        cout<<word[1]<<"is the parameter."<<endl;
+        fflush(stdin);
+        cin>>input;
+    }
+    }
+}
+
+void checkParameter(int& length){
+    while(length<2||length>5){
+        cout<<"please input the length,no less than 3 and no more than 5:";
+        cin>>length;
     }
 }
 
 int main(int argc,char* argv[]){
-    int str[4][4];
-    int s[16];
-    char input[5];
-    int condition=2048;
-    srand((unsigned)time(NULL));
-    if(argv[1]!=NULL&&!strcmp(argv[1],"-t")){
-        condition=64;
-        printf ("enter test mode\n");
-        Sleep(500);
+    Map game_map;
+    int o;
+    const char *optstring = "ts:";
+    while ((o = getopt(argc, argv, optstring)) != -1) {
+        switch (o) {
+            case 't':
+                game_map.setTestMode();
+                break;
+            case 's':
+                if(atoi(optarg)<3||atoi(optarg)>5){
+                    cout<<"please input the length,no less than 3 and no more than 5:";
+                    return 0;
+                }
+                game_map.setLength(atoi(optarg));
+                break;
+            label:case '?':
+                printf("error optopt: %c\n", optopt);
+                printf("error opterr: %d\n", opterr);
+                return 0;
+                break;
+        }
     }
-    print_interface();
-    judge_player();
-    iniMap(str);
-    printMap(str);
+    printInterface();
+    game_map.setMode();
+    if (game_map.getMode()==2){
+        game_map.setUsername();
+    }
+    game_map.iniMap();
+    fflush(stdin);
     while(1){
-        char input[5]; 
-        fflush(stdin);
-        input[0]=getch();
-        fflush(stdin);
-        updateWithInput(str,input,0);
-        c++;
-        judge_player();
-        printMap(str);
-        if (check_win(str,condition)) {
-            printf("you win!\n");
-            break;
+        game_map.printMap();
+        game_map.judgePlayer();
+        game_map.getTurnAddPrintScore();
+        if (game_map.getCount()!=1){
+            if (game_map.check_win()){
+                cout<<"you win!"<<endl;
+                game_map.judgeWinner();
+                break;
+            }
+            else if(game_map.check_lose()){
+                cout<<"you lose."<<endl;
+                game_map.judgeWinner();
+                break;
+            }
         }
-        else if(check_lose(str)){
-            printf("game over!\n");
-            break;
-        }
+        string input;
+        game_map.checkCheated(input);
+        game_map.getCheat(input);
+        game_map.updateWithInput(input,0);
+        fflush(stdin);       
     }
-        if (mode ==2){
-            if (score1>score2)
-            cout<<username1<<"win!"<<endl;
-            else if (score1==score2)
-            cout<<"draw!"<<endl;
-            else
-            cout<<username2<<"win!"<<endl;
-        }
-
     return 0;
-}
+} 
